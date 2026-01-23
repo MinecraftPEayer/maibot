f0ab7f7da68701e2038fa0674f07c04133430376
MinecraftPEayer <boingli227@outlook.com>
2026-01-23 09:12:32 +0000

    feat: /rating新增AP條件計算

--- src/commands/rating.command.ts
+++ src/commands/rating.command.ts
@@ -1,5 +1,5 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
-import { convertAchievementToRank } from 'src/lib/Utils';
+import { calculateRating, convertAchievementToRank } from 'src/lib/Utils';
 import { RankFactor } from 'src/lib/constant/CommonConstant';
 
 const data = new SlashCommandBuilder()
@@ -14,22 +14,27 @@ async function execute(interaction: ChatInputCommandInteraction) {
         constant: string;
         achievement: string;
         rank: string;
+        allPerfect?: boolean;
     }[] = [];
     for (let constant = 1; constant <= 15; constant += 0.1) {
+        const thisResult = [];
         let achievement = 0;
+
+        let canAchieveWithoutAP = false;
 
         let calculateRate = 1;
         while (parseFloat(achievement.toFixed(4)) <= 101) {
             const rating = Math.floor(
                 (parseFloat((achievement > 100.5 ? 100.5 : achievement).toFixed(4)) / 100) *
-                RankFactor[convertAchievementToRank(parseFloat(achievement.toFixed(4)))] *
-                parseFloat(constant.toFixed(1)) *
-                100,
+                    RankFactor[convertAchievementToRank(parseFloat(achievement.toFixed(4)))] *
+                    parseFloat(constant.toFixed(1)) *
+                    100,
             );
             if (rating >= (ratingNeeded ?? 0)) {
                 if (calculateRate === 0.0001) {
-                    if (!result.some((item) => item.achievement === achievement.toFixed(4))) {
-                        result.push({
+                    canAchieveWithoutAP = true;
+                    if (!result.some((item) => item.achievement === achievement.toFixed(4) && !item.allPerfect)) {
+                        thisResult.push({
                             constant: constant.toFixed(1),
                             achievement: achievement.toFixed(4),
                             rank: convertAchievementToRank(parseFloat(achievement.toFixed(4))),
@@ -44,6 +49,17 @@ async function execute(interaction: ChatInputCommandInteraction) {
 
             achievement += calculateRate;
         }
+
+        if (!canAchieveWithoutAP && calculateRating(100.5, constant, true) >= (ratingNeeded ?? 0)) {
+            result.push({
+                constant: constant.toFixed(1),
+                achievement: '100.5000',
+                rank: 'SSS+',
+                allPerfect: true,
+            });
+        } else {
+            result = result.concat(thisResult);
+        }
     }
 
     if (result.length === 0) {
@@ -57,7 +73,7 @@ async function execute(interaction: ChatInputCommandInteraction) {
     for (let item of result) {
         let thisConstant = parseFloat(item.constant);
         let nextConstant = parseFloat(result[index + 1]?.constant);
-        let toAddString = `${`${parseFloat(item.constant).toFixed(1).padStart(4, ' ')}${parseFloat((nextConstant - thisConstant).toFixed(1)) > 0.1 ? `~${(nextConstant - 0.1).toFixed(1)}` : `${index === result.length - 1 ? '~' : ''}`}`.padEnd(9, ' ')} - ${`${parseFloat(item.achievement).toFixed(4).padStart(8, ' ')}%`.padEnd(12, ' ')} (${item.rank})`;
+        let toAddString = `${`${parseFloat(item.constant).toFixed(1).padStart(4, ' ')}${parseFloat((nextConstant - thisConstant).toFixed(1)) > 0.1 ? `~${(nextConstant - 0.1).toFixed(1)}` : `${index === result.length - 1 ? '~' : ''}`}`.padEnd(9, ' ')} - ${(item.allPerfect ? 'ALL PERFECT' : `${parseFloat(item.achievement).toFixed(4).padStart(10, ' ')}%`).padEnd(12, ' ')} (${item.rank})`;
         if ((resultString + toAddString).length > 1900) {
             let leftItemCount = result.length - index;
             resultString += `\n... (還有 ${leftItemCount} 項)\`\`\``;
